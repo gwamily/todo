484011ef0e3f1966ed9b2d98462205482d037dd0
pawix135 <leagueoflegendsoppl@gmail.com>
2025-12-13 02:35:12 +0100

    add base to config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const DEV = true;
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: DEV ? undefined : '/subtodo/',
   plugins: [
     react({
       babel: {
