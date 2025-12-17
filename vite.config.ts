b451c86f4b9ae8befc853ad34b1b7c2253f66bb6
pawix135 <leagueoflegendsoppl@gmail.com>
2025-12-17 02:06:43 +0100

    icon

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
   return {
     base: '/subtodo/',
     plugins: [
